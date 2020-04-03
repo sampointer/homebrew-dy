a07ad9ccfe636901f398cb40a3759816e89776fb
goreleaserbot <sam@outsidethe.net>
2020-04-03 15:20:34 +0100

    Brew formula update for dy version v0.2.1

--- Formula/dy.rb
+++ Formula/dy.rb
@@ -2,16 +2,16 @@
 class Dy < Formula
   desc "Construct YAML from a directory tree"
   homepage "https://github.com/sampointer/dy"
-  version "0.2.0"
+  version "0.2.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sampointer/dy/releases/download/v0.2.0/dy_0.2.0_Darwin_x86_64.tar.gz"
-    sha256 "61883cb973b947c9849fb2aec02a584af9cfda2cfeabe542fc45940bc7eef505"
+    url "https://github.com/sampointer/dy/releases/download/v0.2.1/dy_0.2.1_Darwin_x86_64.tar.gz"
+    sha256 "5971256edd00978909eb063281cb3182baf4d361cd18574f1119f1d60e9b5446"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/sampointer/dy/releases/download/v0.2.0/dy_0.2.0_Linux_x86_64.tar.gz"
-      sha256 "3aab91d20b64a7e09f98f668ca24ce4f7263404e6a1d3efb6b302cb0dbed9fe4"
+      url "https://github.com/sampointer/dy/releases/download/v0.2.1/dy_0.2.1_Linux_x86_64.tar.gz"
+      sha256 "11926458773a5a271863a049fd29711a3d952242a741671b507419a9ae48bb6e"
     end
   end
 
